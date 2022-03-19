5570ca844b8c6c7fb5a273a81341ac29375c2876
Krishnkant patidar <krishnkantpatidar@gmail.com>
2022-03-19 10:52:24 +0530

    pdf design fixed

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -131,7 +131,7 @@ class Test extends CI_Controller
             if (count($data) > 0) {
                 foreach ($data as $dd) {
                     $list .= "<tr><td>" . $dd->test_name . "<input type='hidden' name='testId[]' id='testId' value='" . $dd->id . "' class='form-control testId' readonly></td>
-                    <td><input type='text' name='testAmount[]' id='testAmount' value='" . $dd->amount . "' class='form-control testAmount' readonly><input type='hidden' name='discount_value[]' id='discount_value' value='0'><input type='hidden' name='discountAmount[]' id='discountAmount' value='0' class='form-control testAmount' readonly></td><td><a href='#' class='remove_this btn-danger'><img src='". BASE_URL ."public/assets/images/remove-white.svg' alt=''></a></td></tr>";
+                    <td><input type='text' name='testAmount[]' id='testAmount' value='" . $dd->amount . "' class='form-control testAmount' readonly><input type='hidden' name='discount_value[]' id='discount_value' value='0'><input type='hidden' name='discountAmount[]' id='discountAmount' value='0' class='form-control testAmount' readonly></td><td><a href='#' class='remove_this'><img src='". BASE_URL ."public/assets/images/cross.svg' alt=''></a></td></tr>";
                 }
             }
 
